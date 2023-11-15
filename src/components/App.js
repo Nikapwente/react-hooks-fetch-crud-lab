26f895f9c02d206cfd14a80f03b1af8f4bd79eff
Nikapwente <hrsmajor@gmail.com>
2023-11-15 09:43:06 +0300

    Assignment Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data) => setData(data));
+  }, []);
+
+
+function handleAddNewQuestion(newQuestion) {
+  setData([...data,newQuestion]);
+}
+
+function updateDelete(newList) {
+  setData(newList);
+}
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm updateQuestions={handleAddNewQuestion} showQuestions={setPage}/> : <QuestionList questionData={data} deleted={updateDelete} showQuestions={setPage}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questionData, showQuestions, deleted}) {
+
+  
+  function deleteEntry(event) {
+    console.log(event.target.parentElement.firstChild.firstChild.nextSibling.textContent);
+    const question = event.target.parentElement.firstChild.firstChild.nextSibling.textContent;
+    const newList = questionData.filter((item)=>item.id!=event.target.parentElement.firstChild.firstChild.nextSibling.textContent)
+    
+   
+      
+      fetch(`http://localhost:4000/questions/${question}`, {
+          method: 'DELETE',
+          "Content-Type":"application/json",
+          body: JSON.stringify({
+            "correctIndex": 0
+          })
+        })
+      .then(response => response.json())
+      .then(function() {deleted(newList);
+      })
+
+      // deleted(newList);
+      showQuestions("List");
+  }
+
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionData.map((item) => (<QuestionItem key={item.id} question={item} deleteElement={deleteEntry}/>))}</ul>
     </section>
   );
 }
